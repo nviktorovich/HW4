46c7d4382875951c7d13d5d8b0161cb0a519e4fa
nmaslenko <nvmaslenko@gmail.com>
2021-10-07 20:36:49 +0500

    update

--- main.go
+++ main.go
@@ -1,58 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func getSlice()  (res []int, err error) {
-	var size, num int
-	fmt.Println("Введите целое положительное число - длинну списка")
-
-
-	_, err = fmt.Scan(&size)
-
-	if err != nil || size < 1 {
-		err = errors.New("длина массива должна быть больше 0")
+func getSlice() (numSlice []int, err error) {
+	var input string
+	var num int
+	fmt.Println("Введите числа через пробел")
+	input, err = bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil && err != io.EOF{
+		err = errors.New("некорректный ввод")
 		return
 	}
-	fmt.Println("С новой строки вводите целые числа")
-	for i := 0; i < size; i++ {
-		_, err = fmt.Scan(&num)
+	for _, v := range strings.Split(strings.TrimSpace(input), " ") {
+		num, err = strconv.Atoi(v)
 		if err != nil {
-			err = errors.New("элементом массива должно быть целое число")
+			err = errors.New("необходимо вводить только целые числа")
 			return
 		}
-		res = append(res, num)
+		numSlice = append(numSlice, num)
 	}
+
 	return
 }
 
-func sortSlice(userSlice []int) (sortedSlice []int) {
-	var minPosition int
+
+func sortSlice(inputSlice []int) []int {
+	var tmp []int
+	var min int
 	for {
-		for i := range userSlice {
-			if userSlice[minPosition] > userSlice[i] {
-				minPosition = i
+		flag := true
+		for i := 1; i < len(inputSlice); i++ {
+			if inputSlice[i] < inputSlice[i-1] {
+				min = inputSlice[i]
+				tmp = append(inputSlice[:i], inputSlice[i+1:]...)
+				inputSlice = append([]int{min}, tmp...)
+				flag = false
 			}
 		}
-		sortedSlice = append(sortedSlice, userSlice[minPosition])
-		userSlice = append(userSlice[:minPosition], userSlice[minPosition+1:]...)
-		if len(userSlice) == 0 {
+		if flag {
 			break
-		} else {
-			minPosition = 0
 		}
 	}
-	return
+	return inputSlice
 }
 
+
+
 func main()  {
-	userSlice, err := getSlice()
+	slice, err := getSlice()
 	if err != nil {
-		fmt.Printf("ошибка ввода массива, %s", err)
+		fmt.Printf("ошибка ввода слайса: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Отсортированный срез: %v", sortSlice(userSlice))
+
+	result := sortSlice(slice)
+	if err != nil {
+		fmt.Printf("ошибка сортировки слайса: %s", err)
+	}
+	fmt.Printf("результат: %v", result)
+
 }
